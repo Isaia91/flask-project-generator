0bbc898456f60a0919a5bf3cfbeb245d104e07f4
Isaia91 <isaia.maperi@gmail.com>
2025-03-24 13:28:11 +1100

    recuperation dynamiquement du path pour le script python

--- flask_init.bat
+++ flask_init.bat
@@ -1,9 +1,14 @@
 @echo off
-set mypath=%cd%
+set mypath=%~dp0
+set mypath=%mypath:~0,-1%
+
 if "%1"=="" (
     echo ❌ Erreur : Vous devez spécifier un nom de projet.
     echo Exemple : flask-init MonProjet
     exit /b
 )
+
 set path_to_python_script=%mypath%\flask_init.py
+
 python %path_to_python_script% %1
+
